1a6d44bc0502f8665df99aa269931d453d18f27e
Emanuel Brito da Silva <c92242@santacasaba.local>
2025-08-13 13:08:04 -0300

    Primeiro commit do trablaho

--- src/estudo/controler/IfElse.java
+++ src/estudo/controler/IfElse.java
@@ -3,8 +3,26 @@ package estudo.controler;
 public class IfElse {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/* como eu fiz
+		if (age < 18) {
+			if(age < 16) {
+				System.out.printf("%s, você não pode dirigir.\n", name);
+			}else {
+				System.out.println("Você é emancipado? (s/n)");
+				var isEmancipated = scan.next().equalsIgnoreCase("s");
+				//equals seria o equevalente ==, IgnoreCase ignorar se é maiusculo ou não
+				if(isEmancipated) {
+					System.out.printf("%s apesar de ter %s anos, por ser emancipado você pode dirigir. \n", name, age);
 
+				}else System.out.printf("%s, você não pode dirigir.\n", name);				
+			}
+		}else {
+			System.out.printf("%s tem %s anos, e pode dirigir. \n", name, age);
+		}*/
+		
+		
+		
+		
 	}
 
 }
